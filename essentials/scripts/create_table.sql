811623db2329b5119eaf79ea0516231ea1c83d1e
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-02-27 20:47:28 -0300

    ✨feat(file): exemple to create table with data types

--- essentials/scripts/create_table.sql
+++ essentials/scripts/create_table.sql
@@ -15,3 +15,24 @@ ALTER TABLE IF EXISTS public.tb_collaborators
 
 COMMENT ON TABLE public.tb_collaborators
     IS 'Table exemple';
+
+-- Exemple to create table with data types
+
+CREATE TABLE IF NOT EXISTS public.tb_departament
+(
+    dpt_id integer NOT NULL DEFAULT nextval('tb_departament_dpt_id_seq'::regclass),
+    dpt_name character(100) COLLATE pg_catalog."default" NOT NULL,
+    dpt_type character(10) COLLATE pg_catalog."default",
+    dpt_description character varying(50) COLLATE pg_catalog."default",
+    dpt_comment text COLLATE pg_catalog."default",
+    dpt_creation date,
+    dpt_begin_hour time without time zone,
+    dpt_event timestamp without time zone,
+    dpt_status boolean DEFAULT false,
+    CONSTRAINT dpt_id_pk PRIMARY KEY (dpt_id)
+)
+
+-- TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.tb_departament
+    OWNER to postgres;
